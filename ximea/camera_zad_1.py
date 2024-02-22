fixed rotation

ximea/camera_zad_1.py
@@ -33,14 +33,14 @@ def action_conv(image):
     return cv2.filter2D(src=image, dst=image, ddepth=-1, kernel=custom_kernel)
 
 def action_rotate(image):
-    #image_rot = image.copy()
     for i in range(240):
-        for j in range(240):
-            image[i, j, :] = image[j, i, :]
+        for j in range(i):
+            buff = image[j, i, :].copy()
+            image[j, i, :] = image[i, j, :]
+            image[i, j, :] = buff
     return image
 
 def action_get_red(image):
-    #image_red = image.copy()
     image[:, :, [0, 1, 3]] = 0
     return image
 def nothing(image):
@@ -59,11 +59,12 @@ while cv2.waitKey() != ord('q'):
         cv2.imwrite(f'img_result.jpg', result_img)
         cv2.imshow("result", result_img)
     elif i < 8:
-        #imgs[i % 4] = actions[i % 4](imgs[i % 4])
-        #result_img = cont_imgs(imgs)
-        i_sel = (i % 4) // 2; i_start = i_sel*240
-        j_sel = (i % 4) % 2;  j_start = j_sel*240
-        actions[i % 4](result_img[i_start:(i_start + 240),j_start:(j_start + 240), :])
+        i_sel = (i % 4) // 2
+        i_start = i_sel*240
+        j_sel = (i % 4) % 2
+        j_start = j_sel*240
+
+        actions[i % 4](result_img[i_start:(i_start + 240), j_start:(j_start + 240), :])
         cv2.imwrite(f'img_result_filtered.jpg', result_img)
         cv2.imshow("result", result_img)
     else:
@@ -74,6 +75,7 @@ print('Data acquisition is done...')
 print(f'Data type: {result_img.dtype}, '
       f'shape: {result_img.shape}, '
       f'size: {np.prod(result_img.shape)*result_img.dtype.itemsize} byte')
+
 #stop data acquisition
 print('Stopping acquisition...')
 cam.stop_acquisition()