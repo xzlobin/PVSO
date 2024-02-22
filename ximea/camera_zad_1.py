f5259b4e48a1f2d375f4873d84f9042c1f09f9fd
xzlobin <xzlobin@stuba.sk>
2024-02-22 12:34:01 +0100

    added inplace operations

--- ximea/camera_zad_1.py
+++ ximea/camera_zad_1.py
@@ -30,18 +30,19 @@ cv2.imshow("result", cont_imgs(imgs))
 
 def action_conv(image):
     custom_kernel = np.ones((3, 3), np.float32) / 3
-    return cv2.filter2D(src=image, ddepth=-1, kernel=custom_kernel)
+    return cv2.filter2D(src=image, dst=image, ddepth=-1, kernel=custom_kernel)
 
 def action_rotate(image):
-    image_rot = image.copy()
+    #image_rot = image.copy()
     for i in range(240):
         for j in range(240):
-            image_rot[i, j, :] = image[j, i, :]
-    return image_rot
+            image[i, j, :] = image[j, i, :]
+    return image
+
 def action_get_red(image):
-    image_red = image.copy()
-    image_red[:, :, [0, 1, 3]] = 0
-    return image_red
+    #image_red = image.copy()
+    image[:, :, [0, 1, 3]] = 0
+    return image
 def nothing(image):
     return image
 
@@ -58,8 +59,11 @@ while cv2.waitKey() != ord('q'):
         cv2.imwrite(f'img_result.jpg', result_img)
         cv2.imshow("result", result_img)
     elif i < 8:
-        imgs[i % 4] = actions[i % 4](imgs[i % 4])
-        result_img = cont_imgs(imgs)
+        #imgs[i % 4] = actions[i % 4](imgs[i % 4])
+        #result_img = cont_imgs(imgs)
+        i_sel = (i % 4) // 2; i_start = i_sel*240
+        j_sel = (i % 4) % 2;  j_start = j_sel*240
+        actions[i % 4](result_img[i_start:(i_start + 240),j_start:(j_start + 240), :])
         cv2.imwrite(f'img_result_filtered.jpg', result_img)
         cv2.imshow("result", result_img)
     else:
