e37d0b9f19e1e2028de5dc16def05139fdb8c8c2
xzlobin <xzlobin@stuba.sk>
2024-02-25 04:39:51 +0100

    Trackbars added

--- ximea/Zadanie_2/hough_transform.py
+++ ximea/Zadanie_2/hough_transform.py
@@ -6,4 +6,42 @@ import os
 script_folder = os.path.dirname(os.path.realpath(__file__))
 
 with open(f'{script_folder}/camera_parameters.yaml', 'r') as f:
-    camera_parsed = yaml.load(f)
+    cam_parsed = yaml.safe_load(f)
+    cam_params = {'camera_mat': np.array(cam_parsed['camera_mat']), 
+                  'distortion': np.array(cam_parsed['distortion'])}
+    
+
+minDist = 20
+param1 = 100
+param2 = 100
+minRadius = 0
+maxRadius = 0
+
+img = np.zeros((480,480,4), np.uint8)
+cv2.namedWindow('Camera')
+
+def callback_factory(globa_var_name):
+    def callback(new_value):
+        globals()[globa_var_name] = new_value
+    return callback
+
+# create trackbars for color change
+cv2.createTrackbar('minDist  ' , 'Camera', 0, 100, callback_factory('minDist'))
+cv2.createTrackbar('param1   ' , 'Camera', 0, 200, callback_factory('param1'))
+cv2.createTrackbar('param2   ' , 'Camera', 0, 200, callback_factory('param2'))
+cv2.createTrackbar('minRadius' , 'Camera', 0, 255, callback_factory('minRadius'))
+cv2.createTrackbar('maxRadius' , 'Camera', 0, 255, callback_factory('maxRadius'))
+
+cv2.setTrackbarPos('minDist  ' , 'Camera', minDist)
+cv2.setTrackbarPos('param1   ' , 'Camera', param1)
+cv2.setTrackbarPos('param2   ' , 'Camera', param2)
+
+while True:
+    cv2.imshow('Camera', img)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    print(minDist)
+
+
+cv2.waitKey(0)
